3c64d4a4ff172ca6eec3e606c8f70885d078b5d4
Neil Fordyce <N.Fordyce@dundee.ac.uk>
2013-03-03 00:46:18 +0000

    Gets the "Content" entity from the POST request (i.e. the audio file) and puts it in a byte array.

--- src/com/hacku/swearjar/server/SwearJarServlet.java
+++ src/com/hacku/swearjar/server/SwearJarServlet.java
@@ -1,17 +1,24 @@
 package com.hacku.swearjar.server;
 
+import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Writer;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
@@ -24,75 +31,79 @@ import org.apache.http.impl.client.DefaultHttpClient;
 /**
  * Servlet implementation class SwearJarServlet
  */
-@WebServlet(description = "Converts incoming .wav file to .flac format before sending to Google's ASR.  Sends json response back to app.", urlPatterns = { "/convert" })
+@WebServlet(description = "Converts incoming .wav file to .flac format before sending to Google's ASR.  Sends json response back to app.",
+urlPatterns = {"/convert"})
+@MultipartConfig(maxFileSize = 1024 * 1024 * 1024)
 public class SwearJarServlet extends HttpServlet {
-	
-	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-	
-	/**
-	 * Takes the audio at the specified path and sends it off to Google via HTTP
-	 * POST. Packages the JSON response from Google into a SpeechResponse
-	 * object.
-	 * 
-	 * @param speechFile path to the audio file
-	 * @return SpeechResponse containing recognised speech, null if error occurs
-	 */
-	public static void getSpeechResponse(String speechFile) {
-		try {
-			// Read speech file
-			InputStream inputStream = new FileInputStream(speechFile);
-			ByteArrayInputStream data = new ByteArrayInputStream(
-					IOUtils.toByteArray(inputStream));
+    private static final long serialVersionUID = 1L;
 
-			// Set up the POST request
-			HttpPost postRequest = getPost(data);
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Part part = request.getPart("Content");
+        InputStream inputStream = part.getInputStream();
+        ByteArrayInputStream data = new ByteArrayInputStream(IOUtils.toByteArray(inputStream));
+    }
 
-			// Do the request
-			HttpClient client = new DefaultHttpClient();
-			HttpResponse response = client.execute(postRequest);
+    /**
+     * Takes the audio at the specified path and sends it off to Google via HTTP
+     * POST. Packages the JSON response from Google into a SpeechResponse
+     * object.
+     *
+     * @param speechFile path to the audio file
+     * @return SpeechResponse containing recognised speech, null if error occurs
+     */
+    public static void getSpeechResponse(String speechFile) {
+        try {
+            // Read speech file
+            InputStream inputStream = new FileInputStream(speechFile);
+            ByteArrayInputStream data = new ByteArrayInputStream(
+                    IOUtils.toByteArray(inputStream));
 
-			// Package the returned JSON into a SpeechResponse
-			//SpeechResponse speechResponse = packageResponse(response);
+            // Set up the POST request
+            HttpPost postRequest = getPost(data);
 
-			// Close the stream
-			response.getEntity().getContent().close();
+            // Do the request
+            HttpClient client = new DefaultHttpClient();
+            HttpResponse response = client.execute(postRequest);
 
-			//return speechResponse;
+            // Package the returned JSON into a SpeechResponse
+            //SpeechResponse speechResponse = packageResponse(response);
 
-		} catch (FileNotFoundException ex) {
-			ex.printStackTrace();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
+            // Close the stream
+            response.getEntity().getContent().close();
 
-		//return null;
-	}
+            //return speechResponse;
 
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
 
-	/**
-	 * Sets up the post request
-	 * 
-	 * @param data audio file
-	 * @return HttpPost object with parameters initialised to audio file
-	 */
-	private static HttpPost getPost(ByteArrayInputStream data) {
-		HttpPost postRequest = new HttpPost(
-				"https://www.google.com/speech-api/v1/recognize"
-						+ "?xjerr=1&pfilter=0&client=chromium&lang=en-US&maxresults=1");
+        //return null;
+    }
 
-		// Specify Content and Content-Type parameters for POST request
-		MultipartEntity entity = new MultipartEntity();
-		entity.addPart("Content", new InputStreamBody(data, "Content"));
-		postRequest.setHeader("Content-Type", "audio/x-flac; rate=16000");
-		postRequest.setEntity(entity);
-		return postRequest;
-	}
+    /**
+     * Sets up the post request
+     *
+     * @param data audio file
+     * @return HttpPost object with parameters initialised to audio file
+     */
+    private static HttpPost getPost(ByteArrayInputStream data) {
+        HttpPost postRequest = new HttpPost(
+                "https://www.google.com/speech-api/v1/recognize"
+                + "?xjerr=1&pfilter=0&client=chromium&lang=en-US&maxresults=1");
 
+        // Specify Content and Content-Type parameters for POST request
+        MultipartEntity entity = new MultipartEntity();
+        entity.addPart("Content", new InputStreamBody(data, "Content"));
+        postRequest.setHeader("Content-Type", "audio/x-flac; rate=16000");
+        postRequest.setEntity(entity);
+        return postRequest;
+    }
 }
