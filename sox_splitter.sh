8adea05b6c8c884a529ae98397f35bd60e52c864
Neil Fordyce <N.Fordyce@dundee.ac.uk>
2013-04-05 03:22:56 +0100

    more sox splitter script debugging

--- sox_splitter.sh
+++ sox_splitter.sh
@@ -12,22 +12,22 @@ SILENCE_DIR=`mktemp -d`;
 VAD_DIR=`mktemp -d`;
 
 #Convert to flac
-ffmpeg -i "$1$2" -ar 8000 -sample_fmt s16 "$1$3"
+ffmpeg -i "$1$2" -ar 8000 -sample_fmt s16 "$1$3";
 					
 #Split the input file into several new files at silence
-sox "$1$3" "$SILENCE_DIR/$2$3" silence -l 1 0.1 2% 1 0.2 2% : newfile : restart
+sox "$1$3" "$SILENCE_DIR/$2$3" silence -l 1 0.1 2% 1 0.2 2% : newfile : restart;
 
 #We got the parts we need so remove the full converted file
 rm "$1$3"
 
 #Remove stuff which isn't speech
 for FILENAME in `ls $SILENCE_DIR`; do
-	sox "$SILENCE_DIR/$FILENAME" "$VAD_DIR/$FILENAME" norm vad reverse vad reverse
+	sox "$SILENCE_DIR/$FILENAME" "$VAD_DIR/$FILENAME" norm vad reverse vad reverse;
 done
 
 #Split remaining oversized files
 for FILENAME in `ls $VAD_DIR`; do
-	sox "$VAD_DIR/$FILENAME" "$FILENAME" trim 0 13 : newfile : restart
+	sox "$VAD_DIR/$FILENAME" "$FILENAME" trim 0 13 : newfile : restart;
 done
 
 #Output a list of the files created with their absolute path
