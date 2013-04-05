2bad5e7d275c4bc2411807d868b3d6bd34f57b24
Neil Fordyce <N.Fordyce@dundee.ac.uk>
2013-04-05 17:32:39 +0100

    output of sox commands runs to output file for dianostics

--- sox_splitter.sh
+++ sox_splitter.sh
@@ -15,16 +15,16 @@ SILENCE_DIR=`mktemp -d`;
 VAD_DIR=`mktemp -d`;
 
 #Split the input file into several new files at silence
-sox -t ffmpeg "$1$2" "$SILENCE_DIR/$1$3" silence -l 1 0.1 2% 1 0.2 2% : newfile : restart &> /dev/null
+sox -t ffmpeg "$1$2" "$SILENCE_DIR/$1$3" silence -l 1 0.1 2% 1 0.2 2% : newfile : restart &> output
 
 #Remove stuff which isn't speech
 for FILENAME in `ls $SILENCE_DIR`; do
-	sox "$SILENCE_DIR/$FILENAME" "$VAD_DIR/$FILENAME" norm vad reverse vad reverse &> /dev/null
+	sox "$SILENCE_DIR/$FILENAME" "$VAD_DIR/$FILENAME" norm vad reverse vad reverse &>> output
 done
 
 #Split remaining oversized files
 for FILENAME in `ls $VAD_DIR`; do
-	sox "$VAD_DIR/$FILENAME" "$FILENAME" trim 0 13 : newfile : restart &> /dev/null
+	sox "$VAD_DIR/$FILENAME" "$FILENAME" trim 0 13 : newfile : restart &>> output
 done
 
 #Output a list of the files created with their absolute path
