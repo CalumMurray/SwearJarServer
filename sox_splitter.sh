006123d7a1646a2cbcdf3875ec5b7c6d00af874f
Neil Fordyce <N.Fordyce@dundee.ac.uk>
2013-04-05 02:47:07 +0100

    split script updates

--- sox_splitter.sh
+++ sox_splitter.sh
@@ -8,15 +8,17 @@ then
 fi
 
 #Create temporary directories
-FLAC_FILE=`mktemp --suffix=.flac`;
 SILENCE_DIR=`mktemp -d`;
 VAD_DIR=`mktemp -d`;
 
 #Convert to flac
-ffmpeg -i "$1.$2" -ar 8000 -sample_fmt s16 "$FLAC_FILE"
+ffmpeg -i "$1$2" -ar 8000 -sample_fmt s16 "$1$2"
 					
 #Split the input file into several new files at silence
 sox "FLAC_FILE" "$SILENCE_DIR/$2$3" silence -l 1 0.1 2% 1 0.2 2% : newfile : restart
+
+#We got the parts we need so remove the full converted file
+rm $1$2
 
 #Remove stuff which isn't speech
 for FILENAME in `ls $SILENCE_DIR`; do
@@ -29,11 +31,10 @@ for FILENAME in `ls $VAD_DIR`; do
 done
 
 #Output a list of the files created with their absolute path
-find `pwd` -name "$1.*$3";
+find `pwd` -name "$1*$3";
 
 #remove files which are <= 114 bytes in size
 
 #Delete temporary directories
-rm $FLAC_FILE
 rm -r $SILENCE_DIR
 rm -r $VAD_DIR
