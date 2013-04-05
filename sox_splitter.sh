2a5c312803e7621b9674fcb0928e9b8f1d0623b4
Neil Fordyce <N.Fordyce@dundee.ac.uk>
2013-04-05 16:36:27 +0100

    ignoring output of sox for real now

--- sox_splitter.sh
+++ sox_splitter.sh
@@ -12,23 +12,23 @@ SILENCE_DIR=`mktemp -d`;
 VAD_DIR=`mktemp -d`;
 
 #Split the input file into several new files at silence
-sox -t ffmpeg "$1$2" "$SILENCE_DIR/$1$3" silence -l 1 0.1 2% 1 0.2 2% : newfile : restart
+sox -t ffmpeg "$1$2" "$SILENCE_DIR/$1$3" silence -l 1 0.1 2% 1 0.2 2% : newfile : restart &> /dev/null
 
 #Remove stuff which isn't speech
 for FILENAME in `ls $SILENCE_DIR`; do
-	sox "$SILENCE_DIR/$FILENAME" "$VAD_DIR/$FILENAME" norm vad reverse vad reverse
+	sox "$SILENCE_DIR/$FILENAME" "$VAD_DIR/$FILENAME" norm vad reverse vad reverse &> /dev/null
 done
 
 #Split remaining oversized files
 for FILENAME in `ls $VAD_DIR`; do
-	sox "$VAD_DIR/$FILENAME" "$FILENAME" trim 0 13 : newfile : restart
+	sox "$VAD_DIR/$FILENAME" "$FILENAME" trim 0 13 : newfile : restart &> /dev/null
 done
 
 #Output a list of the files created with their absolute path
-find `pwd` -name "`pwd`$1*$3";
+find `pwd` -name "`pwd`$1*$3"; 
 
 #remove files which are <= 114 bytes in size
 
 #Delete temporary directories
-#rm -r $SILENCE_DIR;
-#rm -r $VAD_DIR;
+rm -r $SILENCE_DIR;
+rm -r $VAD_DIR;
