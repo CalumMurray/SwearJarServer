e3446176ea6d816e543f3bc525991d8a384f841a
Neil Fordyce <N.Fordyce@dundee.ac.uk>
2013-04-05 15:11:55 +0100

    removed ffmpeg call in sox_splitter in favour of ffmpeg lib built into sox

--- sox_splitter.sh
+++ sox_splitter.sh
@@ -11,23 +11,17 @@ fi
 SILENCE_DIR=`mktemp -d`;
 VAD_DIR=`mktemp -d`;
 
-#Convert to flac
-ffmpeg -i "$1$2" -ar 8000 -sample_fmt s16 "$1$3";
-					
 #Split the input file into several new files at silence
-sox "$1$3" "$SILENCE_DIR/$2$3" silence -l 1 0.1 2% 1 0.2 2% : newfile : restart;
-
-#We got the parts we need so remove the full converted file
-rm "$1$3"
+sox -t ffmpeg "$1$3" "$SILENCE_DIR/$2$3" silence -l 1 0.1 2% 1 0.2 2% : newfile : restart
 
 #Remove stuff which isn't speech
 for FILENAME in `ls $SILENCE_DIR`; do
-	sox "$SILENCE_DIR/$FILENAME" "$VAD_DIR/$FILENAME" norm vad reverse vad reverse;
+	sox "$SILENCE_DIR/$FILENAME" "$VAD_DIR/$FILENAME" norm vad reverse vad reverse
 done
 
 #Split remaining oversized files
 for FILENAME in `ls $VAD_DIR`; do
-	sox "$VAD_DIR/$FILENAME" "$FILENAME" trim 0 13 : newfile : restart;
+	sox "$VAD_DIR/$FILENAME" "$FILENAME" trim 0 13 : newfile : restart
 done
 
 #Output a list of the files created with their absolute path
@@ -36,5 +30,5 @@ find `pwd` -name "$1*$3";
 #remove files which are <= 114 bytes in size
 
 #Delete temporary directories
-#rm -r $SILENCE_DIR
-#rm -r $VAD_DIR
+#rm -r $SILENCE_DIR;
+#rm -r $VAD_DIR;
