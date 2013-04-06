cd01ccf4889b752f62a550a22e38eced14c3ccee
Neil Fordyce <N.Fordyce@dundee.ac.uk>
2013-04-06 17:30:08 +0100

    bug fix on number of parameters check

--- sox_splitter.sh
+++ sox_splitter.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 #args check
-if [ $# -ne 3 ]
+if [ $# -ne 4 ]
 then
 	echo "usage: $0 baseDir baseFilename inputExt outputExt"
 	exit 1;
 fi
 
+cd $1
+
 #Make file readable
-sudo chmod +r $1/$2$3
+sudo chmod +r $1$2
 
 #Create temporary directories
 SILENCE_DIR=`mktemp -d`;
@@ -16,10 +18,10 @@ VAD_DIR=`mktemp -d`;
 SOX_PATH=/usr/bin/sox;
 
 #Split the input file into several new files at silence
-echo "$1 $2 $3 $4"
+echo "$1 $2 $3"
 
 echo "removing silence..."
-$SOX_PATH -V6 -t ffmpeg "$1/$2$3" "$SILENCE_DIR/$2$4" silence -l 1 0.1 2% 1 0.2 2% : newfile : restart #&>> /tmp/output
+$SOX_PATH -V6 -t ffmpeg "$1$2" "$SILENCE_DIR/$1$3" silence -l 1 0.1 2% 1 0.2 2% : newfile : restart #&>> /tmp/output
 
 #Remove stuff which isn't speech
 echo "removing non-speech"
@@ -34,7 +36,7 @@ for FILENAME in `ls $VAD_DIR`; do
 done
 
 #Output a list of the files created with their absolute path
-find `pwd` -maxdepth 1 -name "$1/$2*$4"; 
+find `pwd` -maxdepth 1 -name "$1*$3"; 
 
 #remove files which are <= 114 bytes in size
 
