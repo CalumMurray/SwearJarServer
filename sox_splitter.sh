f765bc63698d4e3410fe289b24110383e5c5c899
Neil Fordyce <N.Fordyce@dundee.ac.uk>
2013-04-09 00:20:14 +0100

    adding sox silence splitting back in

--- sox_splitter.sh
+++ sox_splitter.sh
@@ -18,22 +18,19 @@ SOX_PATH=/usr/bin/sox;
 
 #Split the input file into several new files at silence
 ffmpeg -i "$INPUT_FILE" "$TEMP_TRANSCODE"
-$SOX_PATH "$TEMP_TRANSCODE" "$2$4" trim 0 10 : newfile : restart 1>&2
-#$SOX_PATH -t ffmpeg "$2$3" "$2$4" trim 0 10 : newfile : restart 1>&2
-
 
 #Split the input file into several new files at silence
-#$SOX_PATH -t ffmpeg "$2$3" "$SILENCE_DIR/$2$4" silence -l 1 0.1 2% 1 0.2 2% : newfile : restart 1>&2 
+$SOX_PATH "$TEMP_TRANSCODE" "$SILENCE_DIR/$2$4" silence -l 1 0.1 2% 1 0.2 2% : newfile : restart 1>&2 
 
 #Remove stuff which isn't speech
-#for FILENAME in `ls $SILENCE_DIR`; do
-#	$SOX_PATH "$SILENCE_DIR/$FILENAME" "$VAD_DIR/$FILENAME" norm vad reverse vad reverse 1>&2 
-#done
+for FILENAME in `ls $SILENCE_DIR`; do
+	$SOX_PATH "$SILENCE_DIR/$FILENAME" "$VAD_DIR/$FILENAME" norm vad reverse vad reverse 1>&2 
+done
 
 #Split remaining oversized files
-#for FILENAME in `ls $VAD_DIR`; do
-#	$SOX_PATH "$VAD_DIR/$FILENAME" "$FILENAME" trim 0 13 : newfile : restart 1>&2 
-#done
+for FILENAME in `ls $VAD_DIR`; do
+	$SOX_PATH "$VAD_DIR/$FILENAME" "$FILENAME" trim 0 13 : newfile : restart 1>&2 
+done
 
 #Output a list of the files created with their absolute path
 find `pwd` -maxdepth 1 -name "$2*$4";
